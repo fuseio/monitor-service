aa13a16c1687a9f7f2c525e607ec06be9cdef9a2
mul53 <mulengabowa53@gmail.com>
2021-08-30 21:55:50 +0200

    add fixes

--- src/services/fuseswapGraph.js
+++ src/services/fuseswapGraph.js
@@ -2,11 +2,11 @@ const { fuseswapClient } = require('../graphql/client')
 const { tokenPriceQuery } = require('../graphql/queries')
 
 class FuseswapGraphService {
-    async getTokenPrice (address) {
-        const query = tokenPriceQuery(address)
-        const data = await fuseswapClient.request(query)
-        return data?.bundle?.ethPrice * data?.token?.derivedETH
-    }
+  async getTokenPrice (address) {
+    const query = tokenPriceQuery(address)
+    const data = await fuseswapClient.request(query)
+    return data?.bundle?.ethPrice * data?.token?.derivedETH
+  }
 }
 
 module.exports = new FuseswapGraphService()
