541402b4769f8a5a0250934df37ae8aaf1918da1
mul53 <mulengabowa53@gmail.com>
2021-09-04 16:09:23 +0200

    fix merge conflict

--- config/default.js
+++ config/default.js
@@ -1,11 +1,13 @@
 module.exports = {
-  mongo: {
-    debug: true,
-    uri: 'mongodb://localhost/test'
-  },
-  graph: {
-    fuseswapUrl: 'https://graph.fuse.io/subgraphs/name/fuseio/fuseswap'
-  },
-  rpcUrl: 'https://rpc.fuse.io',
-  pollInterval: 21600000 // default 6 hrs
+    api: {
+        port: 3000
+    },
+    mongo: {
+        debug: true,
+        uri: 'mongodb://localhost/test'
+    },
+    graph: {
+        fuseswapUrl: 'https://graph.fuse.io/subgraphs/name/fuseio/fuseswap'
+    },
+    pollInterval: 3600000 // default 1 hour
 }
